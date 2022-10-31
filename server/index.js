77f955d0dce52a19250aaece40cfa7dadd91809b
Moshe Plotkin <mplotkin@jewpaltz.com>
2022-10-31 15:16:56 -0400

    started back on the server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app = express()
 
+const productsController = require('./controllers/products');
+
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Happy Sweet New Year');
+app
+.get('/', (req, res) => {
+    res.status(200).send('Happy Sweet New Year');
 })
+.use('/api/v1/products', productsController)
+
 
 
 app.listen(port, () => {
