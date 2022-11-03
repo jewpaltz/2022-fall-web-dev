9024a6e61249d56f4e4f2355ccea0e5957c36a81
Moshe Plotkin <mplotkin@jewpaltz.com>
2022-11-03 15:13:30 -0400

    Fleshed out the pipeline. Serving static files

--- server/index.js
+++ server/index.js
@@ -6,13 +6,36 @@ const productsController = require('./controllers/products');
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    res.setHeader('SUNY', 'MY SUNY');
+    next();
+
+})
+
+app.use('/', express.static('./client/dist'));
+
 app
 .get('/', (req, res) => {
     res.status(200).send('Happy Sweet New Year');
 })
+.get('/error', (req, res) => {
+    sss.PORT();
+})
 .use('/api/v1/products', productsController)
 
+app.get('*', (req, res) => {
+    
+    res.sendFile('index.html', {root: './client/dist'});
+})
 
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status( err.httpCode ?? 500).send({
+        message: err.message ?? 'Something went wrong',
+        status: err.httpCode ?? 500
+    });
+
+})
 
 app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
