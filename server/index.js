3c90e069f50333444377f128660857890afd8eec
Moshe Plotkin <mplotkin@jewpaltz.com>
2022-11-10 14:38:13 -0500

    added cors handling

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-    res.setHeader('SUNY', 'MY SUNY');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 
 })
