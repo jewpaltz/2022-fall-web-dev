c60ce796960d1a5e5b1da0b0a30e5b3cb5e378a5
Moshe Plotkin <mplotkin@jewpaltz.com>
2022-11-17 15:22:55 -0500

    client - server!!! Cart works on all levels

--- server/models/cart.js
+++ server/models/cart.js
@@ -43,13 +43,14 @@ const update = (userId, productId, quantity) => {
   if (index !== -1) {
     if(quantity === 0) {
         list.splice(index, 1);
+        return "null";
     } else {
         list[index].quantity = quantity;
     }
   }else {
         throw new Error('Cart item not found');
-    }
-  return index;
+  }
+  return { ...list[index], product: getProduct(productId) };
 }
 
 module.exports = { add, get, update }
