e13dbf4b7ecc58e5a68a5b9ce8e43d40c548d34e
Moshe Plotkin <mplotkin@jewpaltz.com>
2022-11-21 14:41:32 -0500

    error handling and loading state created in vm

--- client/src/stores/cart.ts
+++ client/src/stores/cart.ts
@@ -1,7 +1,6 @@
-import myFetch from "@/services/myFetch";
 import { reactive, watch } from "vue";
 import type {  Product } from "./products";
-import session from "./session";
+import session, { api } from "./session";
 
 const PATCH = 'PATCH';
 
@@ -13,14 +12,14 @@ export interface CartItem {
 const cart = reactive([] as CartItem[]);
 
 export function load() {
-    myFetch(`cart/${session.user?.email}`).then((data) => {
+    api(`cart/${session.user?.email}`).then((data) => {
         cart.splice(0, cart.length, ...data as CartItem[]);
     });
 }
 watch(()=> session.user, load);
 
 export function addProductToCart(product: Product, quantity: number = 1) {
-    myFetch(`cart/${session.user?.email}`, { productId: product.id, quantity }).then((data) => {
+    api(`cart/${session.user?.email}`, { productId: product.id, quantity }).then((data) => {
         const i = cart.findIndex((x) => x.product.id === product.id);
         if (i != -1) {
             cart[i] = data as CartItem;
@@ -31,7 +30,7 @@ export function addProductToCart(product: Product, quantity: number = 1) {
 }
 
 export function updateProductQuantity(id: number, quantity: number) {
-    myFetch<CartItem>(`cart/${session.user?.email}/${id}/${quantity}`, {}, PATCH).then((data) => {
+    api<CartItem>(`cart/${session.user?.email}/${id}/${quantity}`, {}, PATCH).then((data) => {
         const i = cart.findIndex((x) => x.product.id === id);
         if (i != -1) {
             if(data){
