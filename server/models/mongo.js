62cd22271b76db82812a9e7a528c75a12fc255b3
Moshe Plotkin <mplotkin@jewpaltz.com>
2022-12-01 14:32:47 -0500

    using .env to direct api calls

--- server/models/mongo.js
+++ server/models/mongo.js
@@ -3,7 +3,9 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = process.env.MONGO_URI ?? "";
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const options = { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }
+
+const client = new MongoClient(uri, options);
 
 module.exports = {
     connect: ()=> client.connect() 
