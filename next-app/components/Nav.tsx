7d34ae7f5c3cda3ec0a42a9c04deee85b47b4f04
Moshe Plotkin <mplotkin@jewpaltz.com>
2022-12-12 14:33:11 -0500

    using hooks for the first time

--- next-app/components/Nav.tsx
+++ next-app/components/Nav.tsx
@@ -9,12 +9,17 @@
     //const isCartOpen = ref(false);
 
 import Link from "next/link"
+import { useState } from "react"
 
     const Cart = ()=> <div></div>
     const LoginBadge = ()=> <div></div>
     const MessageList = ()=> <div></div>
 
 export default ()=>{
+
+    const [isActive, setIsActive] = useState(false);
+    //const isCartOpen = ref(false);
+
     return (
 
     <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
@@ -26,14 +31,14 @@ export default ()=>{
             <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
           </a>
       
-          <a className="navbar-burger { 'is-active': isActive }" click="isActive = !isActive" role="button" aria-label="menu" aria-expanded="false">
+          <a className={`navbar-burger ${ isActive ? 'is-active' : '' }`} onClick={()=> setIsActive( !isActive ) } role="button" aria-label="menu" aria-expanded="false">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
           </a>
         </div>
       
-        <div id="navbarBasicExample" className="navbar-menu { 'is-active': isActive }">
+        <div id="navbarBasicExample" className={`navbar-menu ${ isActive ? 'is-active' : '' }`}>
           <div className="navbar-start">
             <Link href="/" className="navbar-item">
               Home
