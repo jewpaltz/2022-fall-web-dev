b3e4a32315661bddefd4d9841da66d76038cca78
Moshe Plotkin <mplotkin@jewpaltz.com>
2022-12-12 15:28:38 -0500

    react fully listing products

--- next-app/pages/_app.tsx
+++ next-app/pages/_app.tsx
@@ -1,5 +1,5 @@
 import 'bulma';
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import NavVue from '../components/Nav'
 
---
+++ next-app/stores/products.ts
@@ -0,0 +1,44 @@
+import api from "../services/myFetch";
+
+
+
+export function getProducts() {
+  return api<ListEnvelope<Product>>('products');
+}
+
+export function getProduct(id: string) {
+  return api<Product>(`products/${id}`)
+}
+
+export function addProduct(product: Product) {
+    return api<Product>(`products`, product);
+}
+
+export function updateProduct(id: string, product: Product) {
+    return api<Product>(`products/${id}`, product, 'PATCH');
+}
+
+export function deleteProduct(id: string) {
+    return api<{deletedCount:boolean}>(`products/${id}`,{}, 'DELETE');
+}
+
+export interface ListEnvelope<T> {
+    products: T[]
+    total: number
+    skip: number
+    limit: number
+  }
+  
+  export interface Product {
+    _id: string
+    title: string
+    description: string
+    price: number
+    discountPercentage: number
+    rating: number
+    stock: number
+    brand: string
+    category: string
+    thumbnail: string
+    images: string[]
+  }
